584388ff3259314d9135dd182794bf27f9d80847
Max Goldman <maxg@meet.mit.edu>
2010-07-12 18:27:53 +0300

    Some packages

--- late_command.sh
+++ late_command.sh
@@ -7,6 +7,9 @@ echo '%lab-admins ALL= ALL' >> /etc/sudoers
 # Network
 apt-get remove --yes --purge network-manager
 
+# Old GDM
+apt-get --yes install gdm gnome gnome-desktop-environment
+
 # Configuration files
 GIT=$(mktemp -d)
 git clone --no-checkout https://github.com/meet/config-solarize-debian-squeeze.git "$GIT"

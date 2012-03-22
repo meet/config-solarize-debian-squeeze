10ee76fa95ed69b71487362031e1467d4aa562f7
Max Goldman <maxg@meet.mit.edu>
2012-03-22 18:56:40 +0200

    Upgrade Eclipse to 3.7, upgrade Subclipse, and install Google App Engine plugins

--- root/config-eclipse.sh
+++ root/config-eclipse.sh
@@ -4,9 +4,10 @@ set -evx
 
 mkdir -p /opt/MEET/software
 
-release=helios
-variant="jee-$release-linux-gtk"
+release=indigo
+version=SR2
+variant="jee-$release-$version-linux-gtk"
 mirror="http://mirror.switch.ch/eclipse/technology/epp/downloads"
-url="$mirror/release/$release/R/eclipse-$variant.tar.gz"
+url="$mirror/release/$release/$version/eclipse-$variant.tar.gz"
 
 ( cd /opt/MEET/software && wget -N $url )
